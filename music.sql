28d0c6e0a3bb9d1a3434ba7a5b4287634aadfdc8
tasnimaahsan <99555875+tasnimaahsan@users.noreply.github.com>
2022-11-06 21:36:45 -0500

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,1 +1,22 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS songs;
 
+CREATE TABLE artists(
+    artist_id INTEGER PRIMARY KEY,
+    artist_name TEXT
+    );
+
+CREATE TABLE albums(
+    album_id INTEGER PRIMARY KEY,
+    album_name TEXT,
+    artist_name TEXT
+    );
+
+CREATE TABLE songs(
+    songs_id INTEGER PRIMARY KEY,
+    song_name TEXT,
+    album_name TEXT,
+    track_number INTEGER,
+    track_length INTEGER
+    );
